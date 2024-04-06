8617b891316b831ce71dbe2456ce330f04411684
9ziggy9 <davidarogers@protonmail.com>
2024-04-06 01:38:17 -0700

    string demonstration

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include "array.h"
 
 int main(void) {
-  ARRAY(int) xs = ARRAY_INIT(DYNAMIC, 0);
-  ARRAY(int) ys = ARRAY_INIT(DYNAMIC, 0);
+  ARRAY(char *) xs = ARRAY_INIT(DYNAMIC, 0);
+  ARRAY(char *) ys = ARRAY_INIT(DYNAMIC, 0);
 
-  for (int x = 0; x < 10; ++x) ARRAY_PUSH(&xs, x);
-  for (int y = 10; y > 0; y--) ARRAY_PUSH(&ys, y);
+  ARRAY_PUSH(&xs, "hello");
+  ARRAY_PUSH(&xs, "world");
+  ARRAY_PUSH(&ys, "goodbye");
+  ARRAY_PUSH(&ys, "moon");
 
   ARRAY_CAT(&xs, &ys);
 
-  for (size_t i = 0; i < xs.len; i++) printf("%d\n", xs.elems[i]);
-
+  for (size_t i = 0; i < xs.len; i++) printf("%s\n", xs.elems[i]);
   return 0;
 }
