4fa12dc4fe907a131b49e542d0f3035be86410cc
9ziggy9 <davidarogers@protonmail.com>
2024-04-08 17:50:20 -0700

    macro pitfall

--- main.c
+++ main.c
@@ -1,17 +1,45 @@
-#include "array.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+  void *data;
+  size_t datasz;
+  struct Node *next;
+} Node;
+
+typedef struct {
+  Node *head;
+} List;
+
+#define LIST_CREATE() _list_create();
+List _list_create(void) { return (List) { .head = NULL }; }
+
+#define NODE_CREATE(T, V) ({ T _x = V; _node_create(&_x, sizeof(T)); });
+Node *_node_create(void *data, size_t sz) {
+  Node *nn = malloc(sizeof(Node));
+  nn->datasz = sz;
+  nn->data = malloc(sz);
+  memcpy(nn->data, data, sz);
+  return nn;
+}
+
+#define LIST_APPEND(LST, NN)                        \
+  do {                                              \
+    if ((LST)->head == NULL) (LST)->head = NN;      \
+    else {                                          \
+      Node *curr = (LST)->head;                     \
+      while (curr->next != NULL) curr = curr->next; \
+      curr->next = (NN);                            \
+    }                                               \
+  } while(0);                                       \
 
 int main(void) {
-  ARRAY(char *) xs = ARRAY_INIT(DYNAMIC, 0);
-  ARRAY(char *) ys = ARRAY_INIT(DYNAMIC, 0);
+  List lst = LIST_CREATE();
+  Node *nn1 = NODE_CREATE(int, 1);
 
-  ARRAY_PUSH(&xs, "hello");
-  ARRAY_PUSH(&xs, "world");
+  LIST_APPEND(&lst, NODE_CREATE(int, 2));
 
-  ARRAY_PUSH(&ys, "goodbye");
-  ARRAY_PUSH(&ys, "moon");
-
-  ARRAY_CAT(&xs, &ys);
-
-  for (size_t i = 0; i < xs.len; i++) printf("%s\n", xs.elems[i]);
   return 0;
 }
