ef3b55cb4202614b88b41be1cdbe4ccc21828f6e
9ziggy9 <davidarogers@protonmail.com>
2024-04-09 20:34:28 -0700

    hw up

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
+#include <string.h>
 
 void ui_init(void) {
   initscr();
   noecho();
   curs_set(1);
+  set_escdelay(0); // immediate escape key response
 }
 
 #define LEN_INPUT_BUFFER 280
@@ -21,7 +23,6 @@ int main(void) {
   WINDOW *w_out = newwin(screen_height - 4, screen_width - 2, 1, 1);
   assert(w_out != NULL && "uh oh newwin() failed.");
   box(w_out, 0, 0);
-  mvwprintw(w_out, 1, 1, "Hello, world!");
 
   WINDOW *w_in = newwin(3, screen_width - 2, screen_height - 3, 1);
   box(w_in, 0, 0);
@@ -35,9 +36,24 @@ int main(void) {
   char input_buffer[LEN_INPUT_BUFFER] = {0};
   size_t cursor_position = 0;
 
-  while((ch = wgetch(w_in)) != 'q') {
-    if (cursor_position < LEN_INPUT_BUFFER) {
-      input_buffer[cursor_position++] = ch;
+  /*
+    TODO: implement output_buffer which keeps a line number
+    (analogous to cursor position) and stores input buffer output
+    in a char[MAX_LINES][LEN_INPUT_BUFFER] (an array of strings)
+  */
+
+  #define KEY_ESC 27
+  while((ch = wgetch(w_in)) != KEY_ESC) {
+    if (ch == '\n') {
+      mvwprintw(w_out, 1, 1, "%s", input_buffer);
+      memset(input_buffer, 0, LEN_INPUT_BUFFER);
+      cursor_position = 0;
+      wmove(w_in, 1, 3);
+      wclrtoeol(w_in);
+      wrefresh(w_in);
+      wrefresh(w_out);
+    } else if (cursor_position < LEN_INPUT_BUFFER) {
+      input_buffer[cursor_position++] = (char) ch;
     }
     mvwprintw(w_in, 1, 3, "%s", input_buffer);
     wrefresh(w_in);
