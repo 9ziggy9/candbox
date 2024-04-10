d87fc1ddbf88193bd706e09dfdbcec624beb4836
9ziggy9 <davidarogers@protonmail.com>
2024-04-09 17:57:38 -0700

    toolchain

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
-#define LIST_IMPL
-#include "list.h"
-
 int main(void) {
-  List lst = LIST_CREATE();
-  LIST_APPEND(&lst, NODE_CREATE(int, 1));
-  LIST_APPEND(&lst, NODE_CREATE(int, 2));
-  LIST_APPEND(&lst, NODE_CREATE(int, 3));
+  const char *str = "hello";
   return 0;
 }
