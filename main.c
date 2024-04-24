791eb305d4c5cd119f9922f3071fb3750d10ec32
9ziggy9 <davidarogers@protonmail.com>
2024-04-23 21:24:11 -0700

    goto/longjmp demonstration

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+void exit_handler(int exit_code, void *args) {
+  (void) args;
+  if (exit_code == EXIT_FAILURE) printf("EXIT FAILURE\n");
+}
+
+void i_always_fail(void) {
+  exit(EXIT_FAILURE);
+}
 
 int main(void) {
-  const char *abc = "abcdefghijklmnop";
-  while (*abc != '\0') printf("ASCII: %d\n", *abc++);
+  on_exit(exit_handler, NULL);
+  initscr();
+
+_loop:
+  getch();
+  printf("Hello in loop!");
+  goto _loop;
+
   return 0;
 }
